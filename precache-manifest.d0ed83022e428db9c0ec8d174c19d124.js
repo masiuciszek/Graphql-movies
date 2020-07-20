da3925f9c939b8d8078fea5dc19865b8b571f1ff
Masiu <ciszekmarcell@gmail.com>
2020-07-20 13:41:44 +0200

    Updates

---
+++ precache-manifest.d0ed83022e428db9c0ec8d174c19d124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d923b7a0b0b2da27ec70b80d5aac3d0",
+    "url": "/masiuciszek/hangman-game/index.html"
+  },
+  {
+    "revision": "5390b5e5f17d5e4a4397",
+    "url": "/masiuciszek/hangman-game/static/js/2.d4c5d67c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/masiuciszek/hangman-game/static/js/2.d4c5d67c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc0af1fe32d09ad15c9",
+    "url": "/masiuciszek/hangman-game/static/js/main.244a63fd.chunk.js"
+  },
+  {
+    "revision": "a86e37f65a2ff10ceb40",
+    "url": "/masiuciszek/hangman-game/static/js/runtime-main.32b62510.js"
+  },
+  {
+    "revision": "af4824cd8211a3414c08bd7befc65e29",
+    "url": "/masiuciszek/hangman-game/static/media/bg.af4824cd.png"
+  }
+]);
